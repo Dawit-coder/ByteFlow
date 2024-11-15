1a1d63850083e28dd1f5189a666d97cffded0fd2
Dawit-coder <dawit3945@gmail.com>
2024-11-15 19:23:00 +0300

    Build Footer component and set up layout

--- Client/byteflow-client/src/Pages/Home/Home.jsx
+++ Client/byteflow-client/src/Pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React, { useContext } from 'react'
 import styles from './Home.module.css'
 import { Appstate } from '../../App'
 import Header from '../../Components/Header/Header'
+import Footer from '../../Components/Footer/Footer'
 
 function Home() {
   const {user} = useContext(Appstate)
   return (
     <div>
       <Header/>
+      <Footer/>
     </div>
   )
 }
