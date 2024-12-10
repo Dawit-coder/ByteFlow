2530f0c8857065b347cb34c680c7f3cc7b284ae0
Dawit-coder <dawit3945@gmail.com>
2024-12-10 23:57:09 +0300

    Improve Footer and QuestionDetail page UI

--- Client/byteflow-client/src/Components/Footer/Footer.jsx
+++ Client/byteflow-client/src/Components/Footer/Footer.jsx
@@ -1,24 +1,14 @@
 import React from 'react'
 import styles from './Footer.module.css'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 function Footer() {
   return (
     <footer className={styles.container}>
-      <div className={styles.content}>
-        <div className={styles.contact}>
-            <h3>Contact</h3>
-            <p>09......</p>
-            <p>dawit@gmail.com</p>
-        </div>
-        <div className={styles.service}>
-            <h3>Services</h3>
-            <a href="#">About</a>
-        </div>
-        <div className={styles.social}>
-            <h3>Social</h3>
-            <a href="https://www.instagram.com/novawhisperer/" target='blank'>instagram</a>
-            <a href="https://www.linkedin.com/in/dawitteshome/" target='blank'>linkedin</a>
-        </div>
+      <div className={styles.social}>
+          <a href="https://www.instagram.com/novawhisperer/" target='blank'><InstagramIcon/></a>
+          <a href="https://www.linkedin.com/in/dawitteshome/" target='blank'><LinkedInIcon/></a>
       </div>
       <div className={styles.copyright}>
         <p>&copy; {new Date().getFullYear()} ByteFlow Forum. All Rights Reserved.</p>
--- Client/byteflow-client/src/Pages/QuestionDetail/QuestionDetail.jsx
+++ Client/byteflow-client/src/Pages/QuestionDetail/QuestionDetail.jsx
@@ -3,9 +3,13 @@ import axios from '../../axiosConfig'
 import { useParams } from 'react-router-dom';
 import styles from './QuestionDetail.module.css'
 import { Appstate } from '../../App';
+import Layout from '../../Components/Layout/Layout';
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+import PersonIcon from '@mui/icons-material/Person';
 
 function QuestionDetail() {
   const { user } = useContext(Appstate)
+  console.log(user, "user conole")
   const { questionId } = useParams();
   const [question, setQuestion] = useState([]);
   const [answers, setAnswer] = useState([]);
@@ -72,11 +76,12 @@ function QuestionDetail() {
   }
   
   return (
-    <div>
+    <Layout>
         <div>
           {
             question.map((question)=>(
               <div key={question.questionid} className={styles.header}>
+                <div className={styles.arrow}><ArrowRightAltIcon style={{fontSize:30}}/></div>
                 <h1>{question?.title}</h1>
                 <hr />
                 <h4>{question.description}</h4>
@@ -85,14 +90,17 @@ function QuestionDetail() {
           }
         </div>
         <div className={styles.answer_container}>
+          <hr />
           <h1>Answers from the community</h1>
+          <hr />
           <div className={styles.answers_container}>
             <div className={styles.answers_section}>
-              {answers.length > 0 ? (
+              {answers?.length > 0 ? (
                 answers.map((answer)=>(
                   <div className={styles.answer_card} key={answer.key}>
                     <div className={styles.user}>
-                      <p>user</p>
+                      <PersonIcon style={{fontSize:40}}/>
+                      <p>{user.username}</p>
                     </div>
                     <div className={styles.answer}>
                       <p>{answer?.answer}</p>
@@ -102,19 +110,18 @@ function QuestionDetail() {
 
               ) : (
                 <p>No answer yet.</p>
-              )};
-              
+              )}
             </div>
             <div className={styles.answer_input_container}>
               <form onSubmit={handleSubmit}>
                 <textarea placeholder='Write your answer here' onChange={handleTyping} required></textarea>
-                <button type='submit'>Post Answer</button>
+                <button type='submit' className={styles.btn}>Post Answer</button>
               </form>
             </div>
           </div>
         </div>
     
-    </div>
+    </Layout>
   )
 }
 
