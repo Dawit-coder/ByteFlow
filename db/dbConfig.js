ab267ed8571f14eb574563fd0ffb608ab400979d
Dawit-coder <dawit3945@gmail.com>
2024-12-28 19:43:23 +0300

    update the dbConfig variable names

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,11 +1,11 @@
 const mysql2 = require("mysql2");
 
 const dbconnection = mysql2.createPool({
-    user: process.env.USER,
-    database: process.env.DATABASE,
-    host: process.env.HOST,
-    port: process.env.PORT,
-    password: process.env.PASSWORD,
+    user: process.env.MYSQL_USER,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    password: process.env.MYSQL_PASSWORD,
     connectionLimit: 10,
     ssl: {
         rejectUnauthorized: false, 
